6d9510c75828c02d8fd15119e895ea93d8fe6ea4
Touhid Mia <touhid@bitmascot.com>
2020-01-08 17:13:50 +0600

    Task

--- src/main/java/com/hmtmcse/parser4java/ObjectProcessor.java
+++ src/main/java/com/hmtmcse/parser4java/ObjectProcessor.java
@@ -1,5 +1,6 @@
 package com.hmtmcse.parser4java;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
@@ -8,6 +9,7 @@ public class ObjectProcessor {
 
     public static  <T> T mapToObject(Map map, Class<T> klass){
         final ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper.convertValue(map, klass);
     }
 
