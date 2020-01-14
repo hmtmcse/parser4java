a8edcd3578ed7717cafc5f6ac5d7acb5dd83c139
Touhid Mia <touhid@bitmascot.com>
2020-01-14 13:19:29 +0600

    Swagger

--- src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
+++ src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
@@ -1,7 +1,20 @@
 package com.hmtmcse.parser4java;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hmtmcse.parser4java.common.Parser4JavaException;
+
 public class JsonProcessor {
 
+    public String klassToString(Object data) throws Parser4JavaException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            return mapper.writeValueAsString(data);
+        } catch (Exception e) {
+            throw new Parser4JavaException(e.getMessage());
+        }
+    }
 
 }
