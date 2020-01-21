5ab6252e89d45f9eda71bb6c8b90cd8f23984b97
Touhid Mia <touhid@bitmascot.com>
2020-01-21 08:28:25 +0600

    update

--- src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
+++ src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
@@ -7,6 +7,8 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmtmcse.parser4java.common.Parser4JavaException;
 
+import java.util.LinkedHashMap;
+
 public class JsonProcessor {
 
     public String klassToString(Object data) throws Parser4JavaException {
@@ -25,6 +27,17 @@ public class JsonProcessor {
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         try {
             return mapper.writeValueAsString(data);
+        } catch (Exception e) {
+            throw new Parser4JavaException(e.getMessage());
+        }
+    }
+
+    public LinkedHashMap objectToMap(Object data) throws Parser4JavaException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        try {
+            return mapper.convertValue(data, LinkedHashMap.class);
         } catch (Exception e) {
             throw new Parser4JavaException(e.getMessage());
         }
