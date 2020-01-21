2aaa58119c1f2be6734cfe29511b35e078532e0f
Touhid Mia <touhid@bitmascot.com>
2020-01-21 08:20:37 +0600

    update

--- src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
+++ src/main/java/com/hmtmcse/parser4java/JsonProcessor.java
@@ -1,7 +1,9 @@
 package com.hmtmcse.parser4java;
 
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hmtmcse.parser4java.common.Parser4JavaException;
 
@@ -10,6 +12,17 @@ public class JsonProcessor {
     public String klassToString(Object data) throws Parser4JavaException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        try {
+            return mapper.writeValueAsString(data);
+        } catch (Exception e) {
+            throw new Parser4JavaException(e.getMessage());
+        }
+    }
+
+    public String objectToJson(Object data) throws Parser4JavaException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
         try {
             return mapper.writeValueAsString(data);
         } catch (Exception e) {
